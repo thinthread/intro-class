d5406148d14234ce977fd70892ce45ca5ae21fef
Grace Durham <gracelee.durham@gmail.com>
2016-07-24 18:23:43 -0700

    exercise 2-2

--- conditionals.py
+++ conditionals.py
@@ -32,19 +32,49 @@
 # else: 
 # 	print "Yeah, it's the weekend!"
 
-myage = 17
-if (myage >= 18 and myage >= 21): 
-	print "I can vote and go to a bar!"
-elif (myage>= 18 or myage==21):
-	print "I can vote, but I can not go to a bar."
-elif(myage >= 18):
-	print "Yay! I can vote!"
-elif (myage == 18):
-	print "Aww, I can not vote."
-elif (myage >= 21):
-	print " I can go to a bar."
-else:
-	print "I can not vote or go to a bar."
+# myage = 17
+# if (myage >= 18 and myage >= 21): 
+# 	print "I can vote and go to a bar!"
+# elif (myage>= 18 or myage==21):
+# 	print "I can vote, but I can not go to a bar."
+# elif(myage >= 18):
+# 	print "Yay! I can vote!"
+# elif (myage == 18):
+# 	print "Aww, I can not vote."
+# elif (myage >= 21):
+# 	print " I can go to a bar."
+# else:
+# 	print "I can not vote or go to a bar."
+
+# even=8
+# if (even==8):
+# 	print " The number 8 is even."
+# else: 
+# # 	print "The number 8 is odd."	
+# odd=8
+# if (odd==8):
+# 	print "The number 8 is odd."
+# else: 
+# # # 	print "The number 8 is even."
+# even=8
+# odd =9
+# if (8==even and 9==even):
+# 	print "Both numbers are even."
+# elif (8==even and 9==odd):
+# 	print "8 is even and 9 is odd."	
+# else:
+# # 	print "Both numbers are odd."	
+# myfavoritecolor="pink"
+# if (myfavoritecolor=="blue"):
+# 	print "My favorite color is blue."
+# else:
+# # 	print "My favorite color is not blue."	
+# myfavoritecolor="black"
+# if (myfavoritecolor=="blue"or myfavoritecolor=="yellow" or myfavoritecolor=="red"): 
+# 	print "My favorite color is primary color."
+# else:
+# 	print "My favorite color is a secondary color."	
+
 
 
 
