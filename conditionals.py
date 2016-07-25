936186207490b8621031a910c7728cc7b2611952
Grace Durham <grace@Timothys-MacBook-Pro.local>
2016-07-24 17:17:28 -0700

    Exercise 2

---
+++ conditionals.py
@@ -0,0 +1,51 @@
+# name1="SGrace Durham" 
+# name2="Stephanie dont know last name" 
+
+# if (name1 > name2):
+#     print"My name is greater!"
+
+# elif (name1 < name2): 
+# 	print "Their name is greater."
+
+# else:  
+# 	print "Our names are equal!"		
+
+# today=24
+# half=15
+# if (today >= half):
+# 	print "Oh, we're halfway there!"
+# else: 
+#     print "The month is still young."	
+
+# today="Sunday"
+
+# if (today=="Monday"):
+# 	print "Yay class day!"
+# elif (today=="Tuesday"):
+#     print "Sigh, it's only Tuesday."	
+# elif (today=="Wednesday"):
+#     print "Humpday!"
+# elif (today=="Thursday"):
+#     print "#tbt"
+# elif (today=="Friday"):
+# 	print "TGIF!"
+# else: 
+# 	print "Yeah, it's the weekend!"
+
+myage = 17
+if (myage >= 18 and myage >= 21): 
+	print "I can vote and go to a bar!"
+elif (myage>= 18 or myage==21):
+	print "I can vote, but I can not go to a bar."
+elif(myage >= 18):
+	print "Yay! I can vote!"
+elif (myage == 18):
+	print "Aww, I can not vote."
+elif (myage >= 21):
+	print " I can go to a bar."
+else:
+	print "I can not vote or go to a bar."
+
+
+
+                    
